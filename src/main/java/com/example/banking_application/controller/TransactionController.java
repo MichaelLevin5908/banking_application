f3acd3538d502be55b2cb724116f465b32e6bdaa
MichaelLevin5908 <michaellevin42@g.ucla.edu>
2024-10-05 11:46:20 -0700

    Fixed get mapping

--- src/main/java/com/example/banking_application/controller/TransactionController.java
+++ src/main/java/com/example/banking_application/controller/TransactionController.java
@@ -8,22 +8,19 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Rectangle;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/bankStatement")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class TransactionController {
 
     private bankStatement bankstatement;
 
-    @GetMapping
+    @GetMapping("/bankStatement")
     public List<Transaction> generateBankStatement(@RequestParam String accountNumber, @RequestParam String startDate, @RequestParam String endDate) throws DocumentException, FileNotFoundException {
         return bankstatement.generateStatement(accountNumber, startDate, endDate);
     }
